73b3a7a01f1778a19e00c049c791e7d437467333
Mohammad Ruhul Amin <67856574+TNTx1995@users.noreply.github.com>
2022-08-08 12:48:59 +0600

    express route

--- app.js
+++ app.js
@@ -1,27 +1,64 @@
+// const express = require('express')
+// const app = express()
+
+// // listen for request 
+
+// const port = 3000
+// app.listen(port,()=>{
+//     console.log("Listening port on 3000")
+// })
+
+// app.get('/',(req,res)=>{
+//     //res.send('<p>Home page</p>');
+//     res.sendFile('./views/index.html',{root:__dirname})
+// })
+// app.get('/about',(req,res)=>{
+//     //res.send('<p>About Page</p>')
+//     res.sendFile('./views/about.html',{root:__dirname})
+// })
+
+// app.get('/about-me',(req,res)=>{
+//     res.redirect('/about')
+// })
+
+// // 404 request 
+// app.use((req,res)=>{
+//     res.status(404).sendFile('./views/404.html',{root:__dirname})
+// })
 const express = require('express')
 const app = express()
 
 // listen for request 
 
 const port = 3000
+
+
+app.set('view engine','ejs')
+
 app.listen(port,()=>{
     console.log("Listening port on 3000")
 })
-
 app.get('/',(req,res)=>{
     //res.send('<p>Home page</p>');
-    res.sendFile('./views/index.html',{root:__dirname})
+    //res.sendFile('./views/index.html',{root:__dirname})
+    res.render('index',{title:"Home Page"})
 })
 app.get('/about',(req,res)=>{
     //res.send('<p>About Page</p>')
-    res.sendFile('./views/about.html',{root:__dirname})
+    //res.sendFile('./views/about.html',{root:__dirname})
+    res.render('about',{title:'About'})
 })
 
 app.get('/about-me',(req,res)=>{
-    res.redirect('/about')
+    res.redirect('/about',{title:'About Me'})
+})
+
+app.get('/blogs/create',(req,res)=>{
+    res.render('create',{title:'Create Blog Now '})
 })
 
 // 404 request 
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname})
+    //res.status(404).sendFile('./views/404.html',{root:__dirname})
+    res.render('404',{title:404})
 })
